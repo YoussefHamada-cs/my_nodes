39991910f89fa430a4c415b014f5d6fd425aeb57
Youssef Hamada <yosefhagag84171@gmail.com>
2024-08-19 16:24:01 +0300

    create add npte cubit

---
+++ lib/cubits/add_note_cubit/notes_state.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+@immutable
+abstract class NotesState {
+
+}
+
+
+class NotesIntial extends NotesState {}
+
+class NotesLo extends NotesState {}
+
+class Notes extends NotesState {}
